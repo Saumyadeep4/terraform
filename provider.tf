bd513393e9c482a0679c73efd1a07de2aa1668f7
Saumyadeep Chakraborty <57912617+Saumyadeep4@users.noreply.github.com>
2025-04-16 09:11:53 +0530

    Remove secrets

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYZHBUCTNBRQP7J7S"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
